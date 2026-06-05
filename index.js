e77a2f5a2f21d07c05438acfa51c3bc18a88da88
Cristianm_dev <65766142+CristianmcDev@users.noreply.github.com>
2026-06-05 14:08:50 +0200

    Routes login solved

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import authRoutes from "./routes/auth.js";
 import eventsRoutes from "./routes/events.js";
 import { dbConnection } from "./database/config.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 // Crear el server
 const app = express();
 
@@ -17,15 +21,18 @@ app.use(cors());
 //Directorio público
 app.use(express.static("public"));
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 // Lectura y parseo del body
 app.use(express.json());
 
 //Rutas
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventsRoutes);
-
-//CRUD: Eventos
-
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
   console.log(`Server Up on port ${4000}`);
